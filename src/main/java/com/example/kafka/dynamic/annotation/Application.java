a7591396a3b059804fd8cc2677b8af6f2dbfbbc0
daiwuxing <706405182@qq.com>
2025-02-20 03:27:08 +0800

    Add main Application class

---
+++ src/main/java/com/example/kafka/dynamic/annotation/Application.java
@@ -0,0 +1,12 @@
+package com.example.kafka.dynamic.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableKafkaDynamicConsumer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
